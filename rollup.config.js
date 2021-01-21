171bb790039458e3dbaec66bb2f9ae623185b1bc
alexpetercharles <hi@alexpetercharles.ch>
2021-01-21 21:59:51 +0100

    chore: rollup bunlde for more configuration options

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import dts from 'rollup-plugin-dts';
+import typescript from '@rollup/plugin-typescript';
+import copy from 'rollup-plugin-copy';
+
+const name = require('./package.json').main.replace(/\.js$/, '');
+
+const bundle = (config) => ({
+  ...config,
+  input: 'src/oury.ts',
+  external: (id) => !/^[./]/.test(id),
+});
+
+export default [
+  bundle({
+    plugins: [
+      typescript(),
+      copy({
+        targets: [
+          { src: 'src/template/', dest: 'dist/' },
+        ],
+      }),
+    ],
+    output: [
+      {
+        file: `${name}.js`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: `${name}.mjs`,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+  }),
+  bundle({
+    plugins: [dts()],
+    output: {
+      file: `${name}.d.ts`,
+      format: 'es',
+    },
+  }),
+];
